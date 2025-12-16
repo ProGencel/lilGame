e85e544fbf3752c33a687f850e5bf646d472e00e
Ahmet Efe Gençel <bloggeromeo@gmail.com>
2025-12-17 00:18:53 +0300

    better Credits

--- core/src/main/java/com/myname/game/scenes/Hud.java
+++ core/src/main/java/com/myname/game/scenes/Hud.java
@@ -82,43 +82,29 @@ public class Hud {
     private void createBasicSkin() {
         skin = new Skin();
 
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Fonts/font.ttf")); // Font dosyanın adı neyse
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Fonts/font.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
 
-        // 1. BOYUT KURALI (Çok Önemli!)
-        // İndirdiğin Pixel Fontun "orijinal boyutu" neyse (genelde 16px veya 32px olur),
-        // buraya onun katlarını yazmalısın. (16, 32, 48 gibi).
-        // Ara değerler (örn: 20px) pixel art fontu bozar ve yamuk gösterir.
         parameter.size = 16;
 
-        // 2. SİHİRLİ AYARLAR (Bulanıklığı Yok Eden Kısım)
-        // "Linear" yumuşatır, "Nearest" keskin bırakır.
-        parameter.minFilter = Texture.TextureFilter.Nearest; // Küçülürken keskin kalsın
-        parameter.magFilter = Texture.TextureFilter.Nearest; // Büyürken keskin kalsın
+        parameter.minFilter = Texture.TextureFilter.Nearest;
+        parameter.magFilter = Texture.TextureFilter.Nearest;
 
-        // Ekstra Garanti: Gölgelendirmeyi kapat (Bazen varsayılan açık olabilir)
         parameter.shadowOffsetX = 0;
         parameter.shadowOffsetY = 0;
 
-        // Türkçe karakter desteği
         parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS + "ığüşöçİĞÜŞÖÇ";
 
-        // Fontu oluştur
         BitmapFont font = generator.generateFont(parameter);
 
-        // 3. FONTU TEMİZLEME (Önemli)
-        // Bazen oluşturduktan sonra bile scale edilirken bozulabilir.
-        // Bunu engellemek için oluşan fontun kendi filtresini de zorluyoruz.
         font.getRegion().getTexture().setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
 
-        // Scale (ölçek) bozulmasını engelle (Her zaman 1.0 olsun)
         font.getData().setScale(1.0f);
 
         generator.dispose();
 
         skin.add("default-font", font);
 
-        // ... Diğer kısımlar (White texture vs.) aynı kalsın ...
         Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
         pixmap.setColor(Color.WHITE);
         pixmap.fill();
@@ -132,37 +118,25 @@ public class Hud {
 
     private void createDialogBox()
     {
-        // 1. Tabloyu oluştur
         dialogTable = new Table();
 
-        // 2. KONUM VE BOYUT (Hayati Kısım)
-        dialogTable.setPosition(0, 0); // Ekranın sol alt köşesine çivile
-        dialogTable.setWidth(stage.getWidth()); // Genişliği ekran kadar olsun
-        dialogTable.setHeight(150); // YÜKSEKLİĞİ ELLE VERİYORUZ! (Burası 0 olduğu için görünmüyordu)
+        dialogTable.setPosition(0, 0);
+        dialogTable.setWidth(stage.getWidth());
+        dialogTable.setHeight(150);
 
-        // 3. İçerik Hizalaması
-        // Tablonun içindeki yazı sol-üstten başlasın
         dialogTable.left().top();
 
-        // 4. Arka Plan
         TextureRegionDrawable background = new TextureRegionDrawable(skin.getRegion("white"));
         dialogTable.setBackground(background.tint(new Color(0, 0, 0, 0.8f)));
 
-        // 5. Label (Yazı)
         dialogLable = new Label("", skin);
-        dialogLable.setWrap(true); // Yazı taşarsa alt satıra geç
+        dialogLable.setWrap(true);
         dialogLable.setAlignment(Align.topLeft);
 
-        // 6. Tabloya Ekleme
-        // pad: Kenarlardan boşluk bırakır
-        // expandX: Yatayda alanı doldur
-        // fillX: Label'ı yatayda sündür
         dialogTable.add(dialogLable).expandX().fillX().pad(20);
 
-        // Başlangıçta gizli
         dialogTable.setVisible(false);
 
-        // Sahneye ekle
         stage.addActor(dialogTable);
     }
 
--- core/src/main/java/com/myname/game/screens/CreditScreen.java
+++ core/src/main/java/com/myname/game/screens/CreditScreen.java
@@ -3,18 +3,18 @@ package com.myname.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.FileHandleResolver;
-import com.badlogic.gdx.assets.loaders.SkinLoader;
-import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.myname.game.Main;
@@ -29,7 +29,6 @@ public class CreditScreen extends ScreenAdapter {
     private Table rootTable;
     private Table contentTable;
     private ScrollPane scrollPane;
-    private TextButton textButton;
     private Stack stack;
     private Table butonTable;
     private Main main;
@@ -43,36 +42,36 @@ public class CreditScreen extends ScreenAdapter {
 
     public void setCredits()
     {
-        credit[0] = "Dog By\nBetul Cetinkaya";
-        credit[1] = "Sword By\nBerat Oncu";
-        credit[2] = "Frog By\nBegum Aysima Yolcu";
-        credit[3] = "Fishes By\nBuse Sultan Ozmenlikan";
-        credit[4] = "Main Screen Buttons By\nAhmet Efe Gencel";
-        credit[5] = "Other assets By\nOtterisk";
-        credit[6] = "Game Designer Ahmet Efe Gencel";
-        credit[7] = "Developer\nAhmet Efe Gencel";
-        credit[8] = "Producer\nAhmet Efe Gencel";
-        credit[9] = "UI Implementation and Designer\nAhmet Efe Gencel";
-        credit[10] = "System Design\nAhmet Efe Gencel";
+        credit[0] = "Dog Designer : Betül Çetinkaya";
+        credit[1] = "Button Designer : Ahmet Efe Gençel";
+        credit[2] = "Frog Designer : Begüm Aysima Yolcu";
+        credit[3] = "Fish Designer : Buse Sultan Özmenlikan";
+        credit[4] = "Sword Designer : Berat Öncü";
+        credit[5] = "Other assets Designer : Otterisk";
+        credit[6] = "Game Designer : Ahmet Efe Gençel";
+        credit[7] = "Developer : Ahmet Efe Gençel";
+        credit[8] = "Producer : Ahmet Efe Gençel";
+        credit[9] = "UI Implementation and Designer : Ahmet Efe Gençel";
+        credit[10] = "System Design : Ahmet Efe Gençel";
     }
 
     @Override
     public void show() {
         viewport = new FitViewport(Constants.V_WIDTH,Constants.V_HEIGHT);
         stage = new Stage(viewport);
-        Gdx.input.setInputProcessor(stage);
 
-        FileHandleResolver resolver = new InternalFileHandleResolver();
+        Gdx.input.setInputProcessor(stage);
 
         manager.load("Gui/geri.png", Texture.class);
         manager.load("Gui/geriHover.png", Texture.class);
-        manager.setLoader(Skin.class, new SkinLoader(resolver));
-        manager.load("Gui/skinComposer.json", Skin.class);
+
+
         manager.finishLoading();
 
         setCredits();
 
-        skin = manager.get("Gui/skinComposer.json", Skin.class);
+        skin = new Skin();
+        createSkin(skin);
 
         rootTable = new Table(skin);
         rootTable.setFillParent(true);
@@ -128,7 +127,7 @@ public class CreditScreen extends ScreenAdapter {
 
 
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.169f,0.187f,0.102f,1);
+        Gdx.gl.glClearColor(87/255f,106/255f,61/255f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         float y = scrollPane.getScrollY();
@@ -151,5 +150,40 @@ public class CreditScreen extends ScreenAdapter {
     public void resize(int width, int height)
     {
         viewport.update(width,height);
+    }
+
+    public void createSkin(Skin skin)
+    {
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Fonts/font.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        parameter.size = 32;
+
+        parameter.shadowOffsetX = 0;
+        parameter.shadowOffsetY = 0;
+
+        parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS + "ığüşöçİĞÜŞÖÇ";
+
+        BitmapFont font = generator.generateFont(parameter);
+
+        font.getRegion().getTexture().setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+
+        font.getData().setScale(1.0f);
+
+        generator.dispose();
+
+        skin.add("default-font", font);
+
+        Color textColor = Color.valueOf("c7dcd0");
+
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(textColor);
+        pixmap.fill();
+        skin.add("white", new Texture(pixmap));
+
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = skin.getFont("default-font");
+        labelStyle.fontColor = textColor;
+        skin.add("default", labelStyle);
     }
 }
